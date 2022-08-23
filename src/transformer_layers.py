79c98d5439bdcdb04a8fe7d930327ae29f6158e2
tongye <791161009@qq.com>
2022-08-23 09:48:16 +0800

    add some code from g2

--- src/transformer_layers.py
+++ src/transformer_layers.py
@@ -21,13 +21,13 @@ class MultiHeadedAttention(nn.Module):
         self.head_count = head_count
         self.model_dim = model_dim
 
-        self.key = nn.Linear(model_dim, head_count * self.head_size)
-        self.query = nn.Linear(model_dim, head_count * self.head_size)
-        self.value = nn.Linear(model_dim, head_count * self.head_size)
+        self.key_project = nn.Linear(model_dim, head_count * self.head_size)
+        self.query_project = nn.Linear(model_dim, head_count * self.head_size)
+        self.value_project = nn.Linear(model_dim, head_count * self.head_size)
 
         self.dropout = nn.Dropout(dropout)
         self.softmax = nn.Softmax(dim=-1)
-        self.output = nn.Linear(model_dim, model_dim)
+        self.output_layer = nn.Linear(model_dim, model_dim)
 
         self.max_relative_position = max_relative_position
         self.use_neg_dist = use_neg_dist
@@ -37,6 +37,54 @@ class MultiHeadedAttention(nn.Module):
     def forward(self, key, value, query, mask=None):
         """
         Compute multi-headed attention.
-        
+        key  [batch_size, seq_len, hidden_size]
+        value[batch_size, seq_len, hidden_size]
+        query[batch_size, seq_len, hidden_size]
+        mask [batch_size, 1, seq_len] (pad position is false)
+
+        return 
+            - output [batch_size, query_len, hidden_size]
+            - attention_output_weights [batch_size, query_len, key_len]
         """
-        pass
+        batch_size = key.size(0)
+        key_len = key.size(1)
+        query_len = query.size(1)
+
+        # project query key value
+        key = self.key_project(key)
+        value = self.value_project(value)
+        query = self.query_project(query)
+
+        #reshape key, value, query 
+        key = key.view(batch_size, -1, self.head_count, self.head_size).transpose(1,2) #[batch_size, head_count, key_len, head_size]
+        value = value.view(batch_size, -1, self.head_count, self.head_size).transpose(1,2)
+        query = query.view(batch_size, -1, self.head_count, self.head_size).transpose(1,2)
+
+        # scale and calculate attention scores
+        query = query / math.sqrt(self.head_size)
+        scores = torch.matmul(query, key.transpose(2,3))
+        # scores [batch_size, head_count, query_len, key_len]
+
+        # apply mask Note: add a dimension to mask, -> [batch_size, 1, 1, key_len]
+        if mask is not None:
+            scores = scores.masked_fill(~mask.unsqueeze(1), float("-inf"))
+        
+        # apply attention dropout
+        attention_weights = self.softmax(scores) # attention_weights [batch_size, head_count, query_len, key_len]
+        attention_probs = self.dropout(attention_weights)
+
+        # get context vector
+        context = torch.matmul(attention_probs, value) # context [batch_size, head_count, query_len, head_size]
+        context = context.transpose(1,2).contiguous().view(batch_size, -1, self.head_count*self.head_size)
+        # context [batch_size, query_len, hidden_size]
+
+        output = self.output_layer(context)
+
+        attention_output_weights = attention_weights.view(batch_size, self.head_count, query_len, key_len).sum(dim=1) / self.head_count
+        # [batch_size, query_len, key_len]
+        return output, attention_output_weights
+
+
+
+
+        
