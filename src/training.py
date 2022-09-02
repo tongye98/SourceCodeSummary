1cf5edf092cf34254575735515e7da071ce53e7a
tongye <791161009@qq.com>
2022-09-02 22:40:46 +0800

    add code from g2

--- src/training.py
+++ src/training.py
@@ -538,5 +538,5 @@ class TrainManager(object):
             return is_better
 
 if __name__ == "__main__":
-    cfg_file = "configs/rencos_python.yaml"
+    cfg_file = "configs/rencos_python_base3.yaml"
     train(cfg_file=cfg_file, skip_test=False)
