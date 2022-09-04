6e51e891c7ee7265dccb9829fd9d0256af2eed7f
tongye <791161009@qq.com>
2022-09-04 22:37:58 +0800

    add code from g2

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
 
 echo "Let's start do something interesting!"
-#python -m src train configs/transformer.yaml
+export CUDA_VISIBLE_DEVICES=0
+python -m src train configs/rencos_python_base4.yaml
