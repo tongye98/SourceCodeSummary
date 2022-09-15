e39f12361a6ce276b40cea9796861e224e61777b
tongye <791161009@qq.com>
2022-09-15 22:10:19 +0800

    add code from g2

--- src/combiners.py
+++ src/combiners.py
@@ -111,6 +111,69 @@ class DynamicCombiner(Combiner):
 
         log_probs = log_probs.view(batch_size, seq_len, vocab_size).contiguous()
         return log_probs
+    
+    def compute_bandwidth(self, hidden:torch.Tensor, searched_hidden:torch.Tensor) -> torch.Tensor:
+        """
+        hidden [batch_size*seq_len, model_dim]
+        searched_hidden [batch_size*seq_len, top_k, model_dim]
+        """
+        mean_hidden = searched_hidden.mean(dim=1)
+        bandwidth = torch.exp(self.bandwidth_estimator(torch.cat([hidden, mean_hidden], dim=-1)))
+        return bandwidth
 
+    def compute_mixing_weight(self, hidden:torch.Tensor, searched_hidden:torch.Tensor, sparse_probs: torch.Tensor)->torch.Tensor:
+        """
+        hidden: [batch_size*seq_len, hidden_size]
+        searched_hidden [batch_size*seq_len, top_k, model_dim]
+        sparse_probs: [batch_size*seq_len, top_k]
+        return
+            mixing_weight [batch_size*seq_len, 1]
+        """
+        merged_hidden = searched_hidden.transpose(1,2).matmul(sparse_probs.unsqueeze(-1)).squeeze(-1)
+        mixing_weight = torch.sigmoid(self.mixing_weight_estimator(torch.cat([hidden, merged_hidden], dim=-1)))
+        return mixing_weight
+
+    def detailed_forward(self, hidden: torch.Tensor, logits: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]:
+        """
+        hidden [batch_size, seq_len, hidden_size]
+        logits [batch_size, seq_len, hidden_size]
+        return 
+        """
+        # reshape hidden and logits for knn retrieval
+        batch_size, seq_len, model_dim = hidden.size()
+        vocab_size = logits.size(-1)
+        hidden = hidden.view(batch_size*seq_len, model_dim)
+        logits = logits.view(batch_size*seq_len, model_dim)
+
+        # retrieval examples from database
+        if self.training:
+            distances, token_indices, searched_hidden = self.database.enhanced_search(hidden.cpu().numpy(),
+                                                        top_k=self.top_k, retrieval_dropout=True)
+        else:
+            distances, token_indices, searched_hidden = self.database.enhanced_search(hidden.cpu().numpy(),
+                                                        top_k=self.top_k, retrieval_dropout=False)
         
+        distances = torch.FloatTensor(distances).to(hidden.device)
+        token_indices = torch.LongTensor(token_indices).to(hidden.device)
+        searched_hidden = torch.FloatTensor(searched_hidden).to(hidden.device)
 
+        # compute dynamic database bandwidth
+        bandwidth = self.compute_bandwidth(hidden, searched_hidden)
+
+        model_based_distribution = F.softmax(logits, dim=-1)
+        vocab_size = model_based_distribution.size(-1)
+        example_based_distribution, sparse_example_based_distribution = self.kernel.compute_example_based_distribution(
+                                                                            distances, token_indices, vocab_size)
+        
+        mixing_weight = self.compute_mixing_weight(hidden, searched_hidden, sparse_example_based_distribution)
+
+        # compute prediction distribution by interpolating between model distribution and database distribution
+        mixed_distribution = (1-mixing_weight)*model_based_distribution + mixing_weight*example_based_distribution
+
+        mixed_distribution = mixed_distribution.view(batch_size, seq_len, vocab_size).contiguous()
+        model_based_distribution = model_based_distribution.view(batch_size, seq_len, vocab_size).contiguous()
+        example_based_distribution = example_based_distribution.view(batch_size, seq_len, vocab_size).contiguous()
+        mixing_weight = mixing_weight.squeeze(-1).view(batch_size, seq_len).contiguous()
+        bandwidth = bandwidth.squeeze(-1).view(batch_size, seq_len).contiguous()
+
+        return mixed_distribution, model_based_distribution, example_based_distribution, mixing_weight, bandwidth
