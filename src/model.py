82fe97e07ba86d2912d84bceaa8862b874a69867
tongye <791161009@qq.com>
2022-09-28 10:37:10 +0800

    add code from g2

--- src/model.py
+++ src/model.py
@@ -1,19 +1,85 @@
+import logging
+import math
+import torch 
 from torch import Tensor, nn
 import torch.nn.functional as F
-from src.retriever import Retriever
-from src.transformer_layers import CopyGenerator, GlobalAttention
-from src.vocabulary import Vocabulary
-from src.embeddings import Embeddings
-from src.encoders import TransformerEncoder
-from src.decoders import TransformerDecoder 
-from src.helps import ConfigurationError
-from src.initialization import Initialize_model
-import logging
+from typing import Dict
 from pathlib import Path
 import numpy as np
+from src.transformer_layers import CopyGenerator, GlobalAttention
+from src.vocabulary import Vocabulary
+from src.encoders import TransformerEncoder
+from src.decoders import TransformerDecoder 
+from src.helps import ConfigurationError, freeze_params 
+from src.initialization import Initialize_model
 from src.loss import XentLoss, CopyGeneratorLoss
+from src.retriever import Retriever
 
 logger = logging.getLogger(__name__)
+
+class Embeddings(nn.Module):
+    def __init__(self, embedding_dim:int=64,
+                 scale: bool=True, vocab_size:int=0,
+                 padding_index:int=1, freeze:bool=False) -> None:
+        """
+        scale: for transformer see "https://zhuanlan.zhihu.com/p/442509602"
+        """
+        super().__init__()
+        self.embedding_dim = embedding_dim
+        self.scale = scale
+        self.vocab_size = vocab_size
+        self.lut = nn.Embedding(vocab_size, self.embedding_dim, padding_idx=padding_index)
+        if freeze:
+            freeze_params(self)
+
+    def forward(self, source: Tensor) -> Tensor:
+        """
+        Perform lookup for input(source) in the embedding table.
+        return the embedded representation for source.
+        """
+        if self.scale:
+            return self.lut(source) * math.sqrt(self.embedding_dim)
+        else:
+            return self.lut(source)
+
+    def __repr__(self):
+        return (f"{self.__class__.__name__}("
+                f"embedding_dim={self.embedding_dim}, "
+                f"vocab_size={self.vocab_size})")
+    
+    def load_from_file(self, embed_path:Path, vocab: Vocabulary) -> None:
+        """
+        Load pretrained embedding weights from text file.
+        - First line is expeceted to contain vocabulary size and dimension.
+        The dimension has to match the model's specified embedding size, the vocabulary size is used in logging only.
+        - Each line should contain word and embedding weights separated by spaces.
+        - The pretrained vocabulary items that are not part of the vocabulary will be ignored (not loaded from the file).
+        - The initialization of Vocabulary items that are not part of the pretrained vocabulary will be kept
+        - This function should be called after initialization!
+        Examples:
+            2 5
+            the -0.0230 -0.0264  0.0287  0.0171  0.1403
+            at -0.0395 -0.1286  0.0275  0.0254 -0.0932        
+        """
+        embed_dict: Dict[int,Tensor] = {}
+        with embed_path.open("r", encoding="utf-8",errors="ignore") as f_embed:
+            vocab_size, dimension = map(int, f_embed.readline().split())
+            assert self.embedding_dim == dimension, "Embedding dimension doesn't match."
+            for line in f_embed.readlines():
+                tokens = line.rstrip().split(" ")
+                if tokens[0] in vocab.specials or not vocab.is_unk(tokens[0]):
+                    embed_dict[vocab.lookup(tokens[0])] = torch.FloatTensor([float(t) for t in tokens[1:]])
+            
+            logging.info("Loaded %d of %d (%%) tokens in the pre-trained file.",
+            len(embed_dict), vocab_size, len(embed_dict)/vocab_size)
+
+            for idx, weights in embed_dict.items():
+                if idx < self.vocab_size:
+                    assert self.embedding_dim == len(weights)
+                    self.lut.weight.data[idx] == weights
+            
+            logging.info("Cover %d of %d (%%) tokens in the Original Vocabulary.",
+            len(embed_dict), len(vocab), len(embed_dict)/len(vocab))
 
 class Transformer(nn.Module):
     """
