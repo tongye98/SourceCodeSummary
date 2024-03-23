78aa49b3b4cdc882c582a4297d284003aa925c44
tongye <791161009@qq.com>
2024-03-23 11:14:24 +0800

    clean and prepare

--- human/script_java/util.py
+++
@@ -1,6 +0,0 @@
-import jsonlines
-
-filename = "java_default_beam.json"
-with open(filename) as f, open("java_default_beam.txt","w") as g:
-    for line in jsonlines.Reader(f):
-        g.write(line["predictions"][0]+'\n')
